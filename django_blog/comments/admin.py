904ace17dc8829b5e3214b9965aaf996cc7d07dd
dingxiao <dingxiao88@163.com>
2018-08-01 14:30:22 +0800

    优化评论后台管理功能

--- django_blog/comments/admin.py
+++ django_blog/comments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Comment
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ['name', 'email', 'text', 'created_time', 'post']
+
 
 # Register your models here.
+admin.site.register(Comment,CommentAdmin)
